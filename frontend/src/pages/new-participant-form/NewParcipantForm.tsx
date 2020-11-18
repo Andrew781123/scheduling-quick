4e270533c53ea32a289ffc20b568344e4eb10461
andrew1123 <leechihoandrew1123@gmail.com>
2020-11-18 19:06:58 +0800

    prepare to do newParticipantForm layout

--- frontend/src/pages/new-participant-form/NewParcipantForm.tsx
+++ frontend/src/pages/new-participant-form/NewParcipantForm.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { RouteComponentProps } from "react-router-dom";
-import { Button } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
 import { EventContext } from "../../context/event-context/EventProvider";
 
 interface routeParams {
@@ -21,7 +21,7 @@ export const NewParcipantForm: React.FC<NewParcipantFormProps> = props => {
     },
     location: {
       //hasFilledInForm is default to false because we can't determine if the user has filled in the form or not if he manually reach this route
-      state: { hasFilledInForm = false }
+      state = { hasFilledInForm: false }
     }
   } = props;
 
@@ -32,7 +32,7 @@ export const NewParcipantForm: React.FC<NewParcipantFormProps> = props => {
   }, []);
 
   const submitForm = () => {
-    if (!hasFilledInForm) {
+    if (!state.hasFilledInForm) {
       localStorage.setItem("HAS_FILLED_IN_FORM", "true");
     }
   };
@@ -42,10 +42,27 @@ export const NewParcipantForm: React.FC<NewParcipantFormProps> = props => {
       <h1>New participant form for evenId: {eventId}</h1>
       {event.info && <h1>{event.info.organizer}</h1>}
       <Button
+        style={{
+          display: "block"
+        }}
         onClick={() => localStorage.setItem("HAS_FILLED_IN_FORM", "true")}
       >
         Set local storage
       </Button>
+      <div>
+        <TextField
+          className='text-input'
+          value={""}
+          name='participantName'
+          placeholder='Enter name'
+          required={true}
+          label='Name'
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+      </div>
+      <div></div>
     </div>
   );
 };
