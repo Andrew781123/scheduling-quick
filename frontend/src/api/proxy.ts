9c8f5fe4e5c87045d8f660d3cbb4fd6a29317cf6
Andrew781123 <leechihoandrew1123@gmail.com>
2020-12-27 00:15:59 +0800

    change api url

--- frontend/src/api/proxy.ts
+++ frontend/src/api/proxy.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: "https://localhost:5000/api",
   headers: {
     "Content-type": "application/json"
   }
