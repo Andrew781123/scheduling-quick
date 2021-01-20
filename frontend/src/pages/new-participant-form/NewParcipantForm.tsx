e366c91efe4e77f4c67103406f9b2a87237eaf37
Andrew781123 <leechihoandrew1123@gmail.com>
2021-01-20 21:59:47 +0800

    opacity 1% automatically added to class

--- frontend/src/pages/new-participant-form/NewParcipantForm.tsx
+++ frontend/src/pages/new-participant-form/NewParcipantForm.tsx
@@ -345,7 +345,7 @@ export const NewParcipantForm: React.FC<NewParcipantFormProps> = props => {
       <Box my={5} />
 
       <div className='new_participant_form_container'>
-        <div className='new_class_name'>
+        <div className='input_block'>
           <h2 className='label primary_label'>Your Info</h2>
           <TextField
             error={nameError}
@@ -362,7 +362,7 @@ export const NewParcipantForm: React.FC<NewParcipantFormProps> = props => {
             }}
           />
         </div>
-        <div className='new_class_name'>
+        <div className='input_block'>
           <h2 className='label primary_label'>
             Your available date and time slots
           </h2>
